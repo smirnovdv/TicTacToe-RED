81f2210659f16b1ad72eaa536a92c3012f4d78e2
smirnovdv <volmondo@gmail.com>
2020-02-01 17:36:56 -0600

    renamed commit

--- src/AI.js
+++ src/AI.js
@@ -28,7 +28,6 @@ let getRightMove = function (iBrd, iPlayer, iTurn) {
             return (turn === player) ? Math.min(...count) : Math.max(...count)
         };
     }
-
     let checkWin = (board) => {
         let b = board;
         if ((b[0] && b[0] === b[1] && b[1] === b[2]) ||
